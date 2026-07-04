fd9307391998a561b7f087d1dd6edd1bb28b9d24
Gautam29013 <jonygautam29013@gmail.com>
2026-07-04 08:52:56 +0530

    feat: implement Counter component with increment and decrement functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import "../style.css"
 
 const Counter = () => {
-  return (
-    <div>Counter</div>
-  )
+   const [count, setCount] = React.useState(0)
+
+   const increment = () => setCount(count + 1)
+   const decrement = () => setCount(count - 1)
+
+    return (
+      <div className="container">
+        <div>
+        <h1 className="number">{count}</h1>
+        </div>
+
+        <div className="button-container">
+          <button onClick={increment} className="increment">+</button>
+          <button onClick={decrement} className="decrement">-</button>
+        </div>
+      </div>
+    )
 }
 
-export default Counter
+export default Counter                 
