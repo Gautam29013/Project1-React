c4fc60c9b65abbf581d043bae68b7ccda588cbba
Gautam29013 <jonygautam29013@gmail.com>
2026-07-04 08:59:14 +0530

    feat: add Todo component as a new feature

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Counter = () => {
    const [count, setCount] = React.useState(0)
 
-   const increment = () => setCount(count + 1)
-   const decrement = () => setCount(count - 1)
+  const increment = () => {setCount(count + 1)}
 
+  const decrement = () => { setCount(count - 1)} 
     return (
       <div className="container">
         <div>
